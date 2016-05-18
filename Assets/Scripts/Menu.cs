02878e84cbc42abd47a91678c5c693c407a3138e
Mauricio Mateus <mauriciomateus_13@hotmail.com>
2016-05-17 23:20:51 -0300

    Código Menu
    Menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+    public void bt_start() {
+        Debug.Log("Mudou de cena");
+    }
+
+    public void bt_opcao(){
+        Debug.Log("Opção ativada");
+    }
+
+    public void bt_exit() {
+        //Debug.Log("Você fechou o jogo");
+        Application.Quit();
+    }
+
+    public void ChamaCenaFases()  {
+        Application.LoadLevel("Fases");
+    }
+
+    public void ChamaVolume(){
+        Application.LoadLevel("Volumes");
+    }
+
+    public void chamaMenu(){
+        Application.LoadLevel("Menu");
+    }
+
+}
