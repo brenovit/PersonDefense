87a4aa5fbd354422bf55cec40ea192d96c6d0b69
Breno Nunes <breno.arantes97@gmail.com>
2016-05-18 02:16:35 -0300

    Painel de Selecionar Torre
    O sistema de selecionar torre ainda não funciona

---
+++ Assets/Scripts/TowerSelect.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+public class TowerSelect : MonoBehaviour {
+
+	public GameObject Tower;
+	private Sprite imagem;
+
+	public void OnMouseDown(){
+		CliqueiNoBagulho ();
+	}
+
+	public void CliqueiNoBagulho(){
+		print ("Clicou em: "+gameObject.name);
+	}
+
+	// Use this for initialization
+	void Start () {
+		GameObject aux = Tower.gameObject.transform.FindChild ("Monster2").gameObject;
+		imagem = aux.GetComponent<SpriteRenderer> ().sprite;
+		this.gameObject.GetComponent<Image> ().sprite = imagem;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown (0)){
+			if(EventSystem.current.IsPointerOverGameObject()){
+				CliqueiNoBagulho ();
+			}
+		}
+	}
+}
