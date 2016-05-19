fcdd5bf8f55c19a6832d0eb3fa5874bf9895f1eb
Breno Nunes <breno.arantes97@gmail.com>
2016-05-19 13:22:39 -0300

    Revert "Att Construir Torre"
    This reverts commit c9c4767286d2ca9734fc2b3a42ecc0ea94b82d6a.

--- Assets/Scripts/TowerSelect.cs
+++ Assets/Scripts/TowerSelect.cs
@@ -2,26 +2,32 @@
 using System.Collections;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+public class TowerSelect : MonoBehaviour {
 
-public class TowerSelect : MonoBehaviour{
-
-	public GameObject[] Slots;
-
+	public GameObject Tower;
 	private Sprite imagem;
 
-	/*public void SelecionarTorre(){
-		GameObject spot = GameObject.Find ("SpotF2");
-		PlaceMonster selectedSpot = spot.GetComponent<PlaceMonster> ();
-		selectedSpot.ConstruirTorre (Tower);
-		GameObject.Find ("SelectTowerPanel").SetActive (false);
-	}*/
-
-
-	public void EncaixarTorres (GameObject[] torres){
-		for(int i = 0; i < torres.Length; i++){
-			Slots [i].GetComponent<Slot> ().TrocaImagem (torres [i]);
-		}
+	public void OnMouseDown(){
+		CliqueiNoBagulho ();
 	}
 
+	public void CliqueiNoBagulho(){
+		print ("Clicou em: "+gameObject.name);
+	}
 
+	// Use this for initialization
+	void Start () {
+		GameObject aux = Tower.gameObject.transform.FindChild ("Monster2").gameObject;
+		imagem = aux.GetComponent<SpriteRenderer> ().sprite;
+		this.gameObject.GetComponent<Image> ().sprite = imagem;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown (0)){
+			if(EventSystem.current.IsPointerOverGameObject()){
+				CliqueiNoBagulho ();
+			}
+		}
+	}
 }
