dac9b2d5be232ee1fcdca377608ffc36151652d4
Breno Nunes <breno.arantes97@gmail.com>
2016-05-19 13:17:51 -0300

    Att Construir Torre
    Agora já é possivel escolher qual torre será construida, e evolui-la,
    falta implementar o sistema de destruição

--- Assets/Scripts/PlaceMonster.cs
+++ Assets/Scripts/PlaceMonster.cs
@@ -1,27 +1,52 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PlaceMonster : MonoBehaviour {
+	public GameObject[] torrePrefab;
+	public GameObject painel;
 
-	public GameObject torrePrefab;
 	private GameObject torre;
+
 	private GameManagerBehaviour gameManager;
+
+	[HideInInspector]	public bool painelAtivo = false;
+	[HideInInspector]	public bool euChamei = false;
+
 
 	void Start() {
 		gameManager = GameObject.Find ("GameManager").GetComponent<GameManagerBehaviour> ();			//o gamemanager vai procurar o gameobject que tiver o componente gamemanagerbehaiour
 	}
 
-	void OnMouseUp(){																					//Quando o mouse clicar no espaço
-		BotarTorre ();
+	void OnMouseUp ()	{
+		if (torre == null) {																					//Quando o mouse clicar no espaço
+			if (painelAtivo)
+				painelAtivo = false;
+			else
+				painelAtivo = true;
+			painel.SetActive (painelAtivo);
+			painel.gameObject.GetComponent<TowerSelect> ().EncaixarTorres (torrePrefab);
+			euChamei = true;
+		}else{
+			BotarTorre (torre);
+		}
 	}
 
-	private void BotarTorre(){
-		if(canPlaceMonster ()) {																		//se puder botar monstro
-			torre = (GameObject)Instantiate (torrePrefab, transform.position, Quaternion.identity); //instancia o monstro definido no mosterprefab, no pasição e rotação do gameobject atual(slot)
+	public void ConstruirTorre (GameObject tower){
+		print ("Torre nome: "+tower.name);
+		painel.SetActive (false);
+		euChamei = false;
+		painelAtivo = false;
+		BotarTorre (tower);
+	}
+
+	private void BotarTorre(GameObject tower){
+		if(canPlaceTower (tower)) {																		//se puder botar monstro
+			torre = (GameObject)Instantiate (tower, transform.position, Quaternion.identity); //instancia o monstro definido no mosterprefab, no pasição e rotação do gameobject atual(slot)
 			AudioSource audiosource = gameObject.GetComponent<AudioSource> (); 							//define uma variavel para o audio
 			audiosource.PlayOneShot (audiosource.clip);													//toca o som de botar item
 			gameManager.Gold -= torre.GetComponent <TowerData> ().CurrentLevel.custo;				//acessa a carteira do jogador e reduz o seu dinheiro, de acordo com o custo de compra do monstro.
-		} else if(canUpgradeMosnter ()) {																//se ja tiver mosntro, verifica se pode evoluir
+		} else if(canUpgradeTower ()) {																//se ja tiver mosntro, verifica se pode evoluir
 			torre.GetComponent <TowerData>().increaseLevel ();										//para o monstro atual, execulta o procedimento de incrementar o level
 			AudioSource audiosource = gameObject.GetComponent<AudioSource> (); 							//define uma variavel para tratar o audio
 			audiosource.PlayOneShot (audiosource.clip);													//toca o som de botar item
@@ -29,13 +54,13 @@ public class PlaceMonster : MonoBehaviour {
 		}
 	}
 
-	private bool canPlaceMonster(){																		//procedimento que vai informar se pode ou não colocar monstro
-		int cost = torrePrefab.GetComponent <TowerData> ().levels [0].custo;							//define o custo, vai até o prefab do monstro acessa a lista de levels dele, na posição 0, retornando o custo
+	private bool canPlaceTower(GameObject tower){																		//procedimento que vai informar se pode ou não colocar monstro
+		int cost = tower.GetComponent <TowerData> ().levels [0].custo;							//define o custo, vai até o prefab do monstro acessa a lista de levels dele, na posição 0, retornando o custo
 		return torre == null && gameManager.Gold >= cost;												//retorna true, se monstro igual a null, isto é, o lugar esta vazio, e o dinheiro atual for maior que o custo
 
 	}
 
-	private bool canUpgradeMosnter() {																	//procedimento que vai informar se pode evoluir um monstro;
+	private bool canUpgradeTower() {																	//procedimento que vai informar se pode evoluir um monstro;
 		if(torre != null){																			//se o local tiver algum monstro
 			TowerData monsterData = torre.GetComponent <TowerData> ();							//cria uma variavel do tipo dados de monstro, que vai receber o monstro que estiver no slot
 			TowerLevel nextLevel = monsterData.getNextLevel ();										//cria uma variavel do tipo de level do monstro, que vai execultar o procedimento de pegar o level atual
