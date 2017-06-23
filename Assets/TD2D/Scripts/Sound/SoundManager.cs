b41529496ff634ba3758eea01864f24d3a2b6614
Breno Nunes <breno.arantes97@gmail.com>
2017-06-23 07:11:48 -0300

    Mudança da arquitetura

--- Assets/TD2D/Scripts/Sound/SoundManager.cs
+++ Assets/TD2D/Scripts/Sound/SoundManager.cs
@@ -2,15 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SoundManager : MonoBehaviour {
+public class SoundManager : MonoBehaviour
+{
+
+	public AudioClip uiSound;
+	AudioSource audioSource;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		audioSource = GetComponent<AudioSource> ();
+	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		
+	}
+
+	public void PlaySoundUI ()
+	{
+		audioSource.PlayOneShot (uiSound, 0.5f);
 	}
 }
